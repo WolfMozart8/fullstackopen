d135ca5869115f1b55a091d5a1043ae75a397080
Felipe <115050091+WolfMozart8@users.noreply.github.com>
2024-07-23 21:47:10 -0400

    part 1: 1.6 - unicafe step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,14 +1,29 @@
 import { useState } from 'react'
 
+const Button = ({text, handler}) => <button onClick={handler}>{text}</button>
+
+const Display = ({text, count}) => <div>{text} {count}</div>
+
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
+  const goodHandler = () => {setGood(good + 1)}
+  const neutralHandler = () => {setNeutral(neutral + 1)}
+  const badHandler = () => {setBad(bad + 1)}
+
   return (
-    <div>
-      code
-    </div>
+    <>
+      <h1>give feedfack</h1>
+      <Button handler={goodHandler} text="good"/>
+      <Button handler={neutralHandler} text="neutral"/>
+      <Button handler={badHandler} text="bad"/>
+      <h1>statics</h1>
+      <Display text="good" count={good}/>
+      <Display text="neutral" count={neutral}/>
+      <Display text="bad" count={bad}/>
+    </>
   )
 }
 
