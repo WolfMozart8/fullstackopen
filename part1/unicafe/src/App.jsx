f5bb4e796d0e95fc738afd4092b734750234be91
Felipe <115050091+WolfMozart8@users.noreply.github.com>
2024-07-24 00:41:53 -0400

    part 1: 1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,16 +2,42 @@ import { useState } from 'react'
 
 const Button = ({text, handler}) => <button onClick={handler}>{text}</button>
 
-const Display = ({text, count}) => <div>{text} {count}</div>
+const Display = ({text, count}) => {
+  if (text === "positive") return (<div>{text} {count} %</div>)
+  else return (<div>{text} {count}</div>)
+}
 
 function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
 
-  const goodHandler = () => {setGood(good + 1)}
-  const neutralHandler = () => {setNeutral(neutral + 1)}
-  const badHandler = () => {setBad(bad + 1)}
+  const [all, setAll] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [positive, setPositive] = useState(0);
+
+  const goodHandler = () => {
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    getStatics(updatedGood, neutral, bad)
+  }
+  const neutralHandler = () => {
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    getStatics(good, updatedNeutral, bad)
+  }
+  const badHandler = () => {
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    getStatics(good, neutral, updatedBad)
+  }
+
+  const getStatics = (good, neutral, bad) => {
+    const newAll = good + neutral + bad
+    setAll(newAll)
+    setAverage((good - bad) / newAll) // exclude neutral (sum 0)
+    setPositive(good / newAll * 100)
+  }
 
   return (
     <>
@@ -23,6 +49,10 @@ function App() {
       <Display text="good" count={good}/>
       <Display text="neutral" count={neutral}/>
       <Display text="bad" count={bad}/>
+
+      <Display text="all" count={all}/>
+      <Display text="average" count={average}/>
+      <Display text="positive" count={positive}/>
     </>
   )
 }
