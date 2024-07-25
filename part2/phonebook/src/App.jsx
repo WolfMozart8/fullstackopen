0df6e8b19541f7cf7dfcdf03078148b16b13f092
Felipe <115050091+WolfMozart8@users.noreply.github.com>
2024-07-25 14:59:05 -0400

    2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from 'react'
 
 function App() {
+  // const [persons, setPersons] = useState([
+  //   { name: 'Arto Hellas', phone: 12345678 }
+  // ])
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: 12345678 }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState("");
 
+  const [search, setSearch] = useState("");
+  const filteredByName = search === ""
+    ? persons
+    : persons.filter(person => person.name.toLowerCase().includes(search.toLowerCase()))
+
+  console.log(filteredByName);
   const addPerson = (event) => {
     event.preventDefault()
     if (checkRepeated()) {
@@ -31,6 +43,10 @@ function App() {
     setNewPhone(event.target.value)
   }
 
+  const searchByName = (event) => {
+    setSearch(event.target.value)
+  }
+
   const checkRepeated = () => {
 
     let isRepeated = false
@@ -49,13 +65,15 @@ function App() {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with <input value={search} onChange={searchByName} /></div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>name: <input value={newName} onChange={newNameHandler}/></div>
         <div>number: <input value={newPhone} onChange={newPhoneHandler} /></div>
         <div><button type="submit">add</button></div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div>{person.name} {person.phone}</div>)}
+      {filteredByName.map(person => <div>{person.name} {person.phone}</div>)}
     </div>
   )
 }
